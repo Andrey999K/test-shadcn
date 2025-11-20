058e101acfdcc7fb3841742e5040ac95737c0e62
Кутузов Андрей <a.kutuzov@2035.university>
2025-11-20 18:50:45 +0300

    fix: fix errors Button and Link

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -27,6 +27,7 @@ const buttonVariants = cva(
         icon: "size-9",
         "icon-sm": "size-8",
         "icon-lg": "size-10",
+        clear: "",
       },
     },
     defaultVariants: {
@@ -39,7 +40,7 @@ const buttonVariants = cva(
 export type ButtonProps = React.ComponentProps<"button"> &
   VariantProps<typeof buttonVariants> & {
   asChild?: boolean
-}
+};
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
@@ -54,6 +55,8 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       />
     )
   }
-)
+);
 
-export { Button, buttonVariants }
+Button.displayName = "Button";
+
+export { Button, buttonVariants };
